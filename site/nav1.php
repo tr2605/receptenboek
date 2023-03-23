2081499d6cfa31890bec0538343b59505fa66880
Tim Rogier <t.rogier@me.com>
2023-03-23 11:31:20 +0100

    commit

--- site/nav1.php
+++ site/nav1.php
@@ -1,5 +1,5 @@
 <div class="topnav">
-        <h1>Recepten van de protugese keuken</h1>
+        <h1>Recepten van de portugese keuken</h1>
         <a class="imger">
             <a href="recept-index.php">Home</a>
             <a href="specials.php">Specials</a>      
