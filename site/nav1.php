af0368d7c9c3dc59b2c179c67ddfb5b78d4960c0
Tim Rogier <t.rogier@me.com>
2023-03-24 11:31:48 +0100

    commit

--- site/nav1.php
+++ site/nav1.php
@@ -2,6 +2,11 @@
         <h1>Recepten van de portugese keuken</h1>
         <a class="imger">
             <a href="recept.php">Home</a>
+            <a href="recepten.php?nummer=1">Cataplana de Marisco</a>
+            <a href="recepten.php?nummer=2">Francesinha</a>
+            <a href="recepten.php?nummer=3">Bacalhau</a>
+            <a href="recepten.php?nummer=4">Pastel de nata</a>
+            <a href="recepten.php?nummer=5">Port</a>
             <a href="specials.php">Specials</a>      
             </div>
         </div>
--- site/nav2.php
+++ site/nav2.php
@@ -1,8 +1,13 @@
 <div class="topnav">
         <h1><?php echo $recept["titel"] ?></h1>
         <a class="imger">
-            <a href="recept.php">Home</a>
-            <a href="specials.php">Specials</a>
+        <a href="recept.php">Home</a>
+            <a href="recepten.php?nummer=1">Cataplana de Marisco</a>
+            <a href="recepten.php?nummer=2">Francesinha</a>
+            <a href="recepten.php?nummer=3">Bacalhau</a>
+            <a href="recepten.php?nummer=4">Pastel de nata</a>
+            <a href="recepten.php?nummer=5">Port</a>
+            <a href="specials.php">Specials</a>      
             </div>
         </div>
     </div>
