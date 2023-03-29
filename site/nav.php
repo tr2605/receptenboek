ac50ba5fd6e5652fbae23502b293d9a390f6a85f
Tim Rogier <t.rogier@me.com>
2023-03-29 09:06:33 +0200

    commit

--- site/nav.php
+++ site/nav.php
@@ -1,14 +1,14 @@
 <div class="topnav">
-    
+
     <h1>Recepten van de portugese keuken</h1>
     <class="imger">
         <div class="navver">
-        <a href="recept.php">Home</a>
-        <?php foreach ($all_recepten as $recept) : ?>
-            <a href="recepten.php?nummer=<?php echo $recept["nummer"] ?>"><?php echo $recept["titel"] ?></a>
-        <?php endforeach; ?>
-        <a href="specials.php">Specials</a>
-        
+            <a href="recept.php">Home</a>
+            <?php foreach ($all_recepten as $recept) : ?>
+                <a href="recepten.php?nummer=<?php echo $recept["nummer"] ?>"><?php echo $recept["titel"] ?></a>
+            <?php endforeach; ?>
+            <a href="specials.php">Specials</a>
+
             <?php
             $sql = "SELECT COUNT(*) as total_numbers FROM database_receptenboek WHERE nummer REGEXP '^[0-9]+$'";
             $result = mysqli_query($conn, $sql);
